589d5199762e5467b11d01dd7829bf24ca9befb8
Cl11922587 <263797420@qq.com>
2019-11-08 10:52:50 +0800

    '提交全描述'

--- src/js/string.js
+++ src/js/string.js
@@ -11,3 +11,32 @@
 *return ：number
 */
 const BYTESIZE = str => new Blob(str).size;
+
+/*
+*首字符大写
+*param ：1.string
+*return ：number
+*/
+const CAPITALIZE = ([first,...rest])=> first.toUpperCase()+rest.join('');
+
+/*
+*首字母小写
+*param ：1.string
+*return ：number
+*/
+const NOCAPITALIZE = ([first,...rest]) => first.toLowerCase()+rest.join('');
+
+/*
+*将-分割的字符串转换成驼峰
+*param ：1.string
+*return ：number
+*/
+const  CAPIALIZEWORD= str =>  str.replace(/\-[a-z]/g,char => char.substr(1,1).toUpperCase());
+
+/*
+*删除字符串中的HTML标签
+*param ：1.string
+*return ：number
+*/
+
+const DELSTRINGHTML = str => str.replace(/<[^>]*>/g,'');
